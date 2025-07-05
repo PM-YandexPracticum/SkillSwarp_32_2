419fb8a007b56cbdd2938b671229e6c062b7ddc7
RilPax <RilPax@yandex.ru>
2025-07-05 23:07:42 +0500

    button

---
+++ src/shared/ui/button/type.ts
@@ -0,0 +1,23 @@
+import type { ReactNode } from "react"
+
+// Сделал такие интерфейсы для того, чтобы не было коллизий между типами: 
+// onClick обязателен если type === button
+// to обязателен если type === link
+
+interface ButtonAsButton {
+  type: 'button'
+  onClick: () => void
+  children: ReactNode
+  className?: string
+  to?: never
+}
+
+interface ButtonAsLink {
+  type: 'link'
+  to: string
+  children: ReactNode
+  className?: string
+  onClick?: never
+}
+
+export type ButtonUIProps = ButtonAsButton | ButtonAsLink
