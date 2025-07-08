0bde2f615094e6618786a3506536370763ad829b
Siblns <lebedevanat95@yandex.ru>
2025-07-09 03:14:33 +0700

    Add DropDrag and stories

---
+++ src/shared/ui/drap-dragUI/index.ts
@@ -0,0 +1,1 @@
+export { DrapDragUI } from './drap-drag';
---
+++ src/stories/DrapDrag.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DrapDragUI } from '@/shared/ui/drap-dragUI';
+
+const meta = {
+  title: 'Example/DrapDrag',
+  component: DrapDragUI,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered'
+  }
+} satisfies Meta<typeof DrapDragUI>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const LoggedIn: Story = {};
