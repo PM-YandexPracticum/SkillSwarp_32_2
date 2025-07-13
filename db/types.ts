d823ea7bfa7ac5f4fcd3eac46a70213bdcb83d84
Pavel-on-GH <Pavel.Zaitsev-7@yandex.ru>
2025-07-14 02:31:09 +0300

    Написал слайс юзера

--- db/types.ts
+++ db/types.ts
@@ -4,96 +4,96 @@
 
 // type subFilterType = string;
 
-// // type filterStatus = 'partial' | 'full' | 'empty'
+type filterStatus = 'partial' | 'full' | 'empty';
 
 // type genderType = null | 'male' | 'female';
 
-// type cityType = string
+type cityType = string;
 
-// type status = 'penging' | 'rejected' | 'fullfiled'
+type status = 'penging' | 'rejected' | 'fullfiled';
 
-// interface incomingType {
-//   userId: string
-//   status: status
-// }
+interface incomingType {
+  userId: string;
+  status: status;
+}
 
-// interface outgoingType {
-//   userId: string
-//   status: status
-// }
+interface outgoingType {
+  userId: string;
+  status: status;
+}
 
-// interface Tskill {
-//   skillTitle: string
-//   type: filterType
-//   subType: subFilterType
-// }
+interface Tskill {
+  skillTitle: string;
+  type: filterType;
+  subType: subFilterType;
+}
 
-// // типы карточек 
+// типы карточек
 
-// export interface TCard {
-//   id: string
-//   userId: string
-//   teachSkill: Tskill[]
-//   learnSkill: Tskill[]
-//   name: string
-//   city: string
-//   age: number
-//   description: string
-//   gender: genderType
-//   createdAt: number
-//   likes: string[]
-//   src: string
-// }
+export interface TCard {
+  id: string;
+  userId: string;
+  teachSkill: Tskill[];
+  learnSkill: Tskill[];
+  name: string;
+  city: string;
+  age: number;
+  description: string;
+  gender: genderType;
+  createdAt: number;
+  likes: string[];
+  src: string;
+}
 
 // // типы компонентов скилла
 
-// export interface TSkillCard {
-//   id: string
-//   userId: string
-//   title: string
-//   description: string
-//   images: string[]
-//   filterType: filterType
-//   subFilterType: subFilterType
-// }
+export interface TSkillCard {
+  id: string;
+  userId: string;
+  title: string;
+  description: string;
+  images: string[];
+  filterType: filterType;
+  subFilterType: subFilterType;
+}
 
 // // типы фильтров
 
-// interface TSubFilter {
-//   id: string
-//   title: string
-//   // isSelected: boolean
-//   type: subFilterType
-//   status:false
-// }
+interface TSubFilter {
+  id: string;
+  title: string;
+  isSelected: boolean;
+  type: subFilterType;
+}
 
-// export interface TFilter {
-//   id: string
-//   type: filterType
-//   title: string
-//   // status: filterStatus
-//   subFilters: TSubFilter[]
-// }
+export interface TFilter {
+  id: string;
+  type: filterType;
+  title: string;
+  status: filterStatus;
+  subFilters: TSubFilter[];
+}
 
 // // тип профиля юзера
 
-// export interface TUser {
-//   gender: genderType
-//   id: string
-//   name: string
-//   city: cityType
-//   age: number
-//   mail: string
-//   password: string
-//   description: string
-//   incoming: incomingType[]
-//   outgoing: outgoingType[]
-//   image: string
-// }
+export interface TUser {
+  gender: genderType;
+  id: string;
+  name: string;
+  city: cityType;
+  age: number;
+  mail: string;
+  password: string;
+  description: string;
+  incoming: incomingType[];
+  outgoing: outgoingType[];
+  image: string;
+  likes: string[];
+}
 
 // // тип города
 
-// export interface TCity {
-//   id: string
-//   title: cityType
-// }
+export interface TCity {
+  id: string;
+  title: cityType;
+}
