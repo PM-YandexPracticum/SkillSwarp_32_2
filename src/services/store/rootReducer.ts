d6f51b7f79aa233a0aff0362553bf4882c576f79
RilPax <RilPax@yandex.ru>
2025-07-13 23:13:21 +0500

    redusers-update

--- src/services/store/rootReducer.ts
+++ src/services/store/rootReducer.ts
@@ -1,5 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { cardsReducer } from '../slices';
 
 export const rootReducer = combineReducers({
   // Сюда добавлять готовые редьюсеры
+  cardsReducer: cardsReducer
 });
