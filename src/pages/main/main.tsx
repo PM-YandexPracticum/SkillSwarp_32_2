fd8399253a7ac2be2f395b3ddfbaee720559c937
Родионцева Ирина Михайловна <I.rodiontseva165@yandex.ru>
2025-07-13 23:40:06 +0300

    added main page

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,7 +1,29 @@
 import type { FC } from 'react';
+import styles from './main.module.css';
+import { CardListUI } from '@/shared/ui';
+import { UserCard } from '@/widgets';
 
 export const Main: FC = () => {
-    return (
-        <></>
-    );
+  return (
+    <main className={styles.main}>
+      <div className={styles.filters}></div>
+      <div className={styles.card_blocks}>
+        <CardListUI title='Популярное' handleOpen={() => {}}>
+          {[...Array(3)].map((_, index) => (
+            <UserCard key={index} />
+          ))}
+        </CardListUI>
+        <CardListUI title='Новое' handleOpen={() => {}}>
+          {[...Array(3)].map((_, index) => (
+            <UserCard key={index} />
+          ))}
+        </CardListUI>
+        <CardListUI title='Рекомендуем'>
+          {[...Array(9)].map((_, index) => (
+            <UserCard key={index} />
+          ))}
+        </CardListUI>
+      </div>
+    </main>
+  );
 };
