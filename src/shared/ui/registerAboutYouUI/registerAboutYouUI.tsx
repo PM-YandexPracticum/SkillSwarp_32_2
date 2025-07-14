2d96b6fe6b1da577ce36d096723f5425b4cb6d76
Siblns <lebedevanat95@yandex.ru>
2025-07-15 03:29:36 +0700

    Fix component

--- src/shared/ui/registerAboutYouUI/registerAboutYouUI.tsx
+++ src/shared/ui/registerAboutYouUI/registerAboutYouUI.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { useState, type FC } from 'react';
 
 import styles from './registerAboutYou.module.css';
 import type { registerAboutYouUIProps } from './type';
@@ -10,31 +10,112 @@ import { ProgressBar } from '@/widgets';
 import { UserCircleSVG } from '@/assets/svg/user-circle';
 import { PlusCircleSVG } from '@/assets/svg/plus-circle';
 import { HugeUserAccountSVG } from '@/assets/svg/huge-user-account';
+import type { DropdownOption } from '../dropdownUI/type';
+import { DropdownUI } from '../dropdownUI';
+import { CheckboxUI } from '../checkboxUI';
 
 export const RegisterAboutYouUI: FC<registerAboutYouUIProps> = ({
   name,
   setName,
-  gender,
-  setGender,
-  age,
-  setAge,
-  city,
-  setCity,
-  skill,
-  setSkill,
+  //gender,
+  //setGender,
+  //age,
+  //setAge,
+  //city,
+  //setCity,
+  //skill,
+  //setSkill,
   handleSubmit
 }) => {
-  /*
-  type genderType = {
-    name: null | 'male' | 'female';
-    id: null | '1' | '2';
-  };
-  const genderOptions: genderType[] = [
-    { name: 'male', id: '1' },
-    { name: 'female', id: '2' },
+
+  const ages: DropdownOption[] = [
+    {id: '20', name: '20'},
+    {id: '21', name: '21'},
+    {id: '22', name: '22'}
   ];
-  const [selectedGender, setSelectedGender] = useState<genderType | null>(null);
-*/
+
+  const [age, setAge] = useState<DropdownOption>({id: '', name: ''});
+
+  const renderAgeOptions = () => {
+    return ages.map(age => (
+      <li key={age.id} onClick={() => setAge(age)}>
+        {age.name}
+      </li>
+    ));
+  };
+
+ const genders: DropdownOption[] = [
+  {id: 'gender_male', name: 'Мужской'},
+  {id: 'gender_female', name: 'Женский'},
+  {id: 'gender_unknown', name: 'Не указан'}
+];
+
+const [gender, setGender] = useState<DropdownOption>({id: '', name: ''});
+
+const renderGenderOptions = () => {
+    return genders.map(gender => (
+      <li key={gender.id} onClick={() => setGender(gender)}>
+        {gender.name}
+      </li>
+    ));
+  };
+
+   const cities: DropdownOption[] = [
+    {id: '1', name: 'Москва'},
+    {id: '22', name: 'Барнаул'},
+    {id: '22', name: 'барнаул'},
+    {id: '3', name: 'Уфа'},
+    { id: '4', name: 'JavaScript' },
+  ];
+
+  const [city, setCity] = useState<DropdownOption>({id: '', name: ''});
+
+  const renderCityOptions = () => {
+    return cities.map(city => (
+      <li key={city.id} onClick={() => setCity(city)}>
+        {city.name}
+      </li>
+    ));
+  };
+
+  const options: DropdownOption[] = [
+    { id: '1', name: 'JavaScript' },
+    { id: '2', name: 'TypeScript' },
+    { id: '3', name: 'React' },
+    { id: '4', name: 'Vue' },
+    { id: '5', name: 'Angular' },
+    { id: '6', name: 'Node.js' },
+    { id: '7', name: 'Python' },
+  ];
+
+  const [checkboxes, setCheckboxes] = useState<DropdownOption[]>([]);
+
+  const handleCheckboxes = (id: string) => {
+      setCheckboxes((prev) => {
+        if (prev.some(item => item.id === id)) {
+          return prev.filter(item => item.id !== id);
+        };
+
+        const option = options.find(option => option.id === id);
+        if (!option) return prev;
+        return [...prev, option];
+      });
+    };
+
+  const renderCheckboxes = (options: DropdownOption[]) => {
+    return options.map((option: DropdownOption) => (
+      <li key={option.id}>
+        <CheckboxUI
+          label={option.name}
+          value={option.id}
+          checked={checkboxes.some((item) => item.id === option.id)}
+          onChange={() => handleCheckboxes(option.id)}
+        />
+      </li>
+      )
+    );
+  };
+
   return (
     <main className={styles.container}>
       <div className={styles.wrapper}>
@@ -64,40 +145,58 @@ export const RegisterAboutYouUI: FC<registerAboutYouUIProps> = ({
               error={false}
               errorText=''
             />
-            <div className={styles.gentwo}>
-              <InputUI
-                label='Возраст'
-                type='text'
-                placeholder='Введите ваш возраст'
-                onChange={(e) => setAge(e.target.value)}
-                value={age}
-                name='Возраст'  
-              />
-              <InputUI
-                label='Пол'
-                type='text'
-                placeholder='Введите ваш пол'
-                onChange={(e) => setGender(e.target.value)}
-                value={gender}
-                name='Gender'
-              />
+            <div className={styles.gentwo}>              
+              <div className={styles.dropdownBlock}>
+                <p>Возраст</p>
+                <DropdownUI 
+                    withFilter={false} 
+                    isMultiSelect={false} 
+                    value={age} 
+                    placeholder='Не указан'
+                  >
+                  {() => renderAgeOptions()}
+                </DropdownUI>
+              </div>
+              <div className={styles.dropdownBlock}>
+                <p>Пол</p>
+                <DropdownUI 
+                    withFilter={false} 
+                    isMultiSelect={false} 
+                    value={gender} 
+                    placeholder='Не указан'
+                  >
+                  {() => renderGenderOptions()}
+                </DropdownUI>
+              </div>
             </div>
-            <InputUI
-                label='Выберите навыки, которым хотите научиться'
-                type='text'
-                placeholder='Выберите категорию навыка'
-                onChange={(e) => setSkill (e.target.value)}
-                value={skill}
-                name='skill'  
-              />
-            <InputUI
-                label='Город'
-                type='text'
-                placeholder='Выберите ваш город'
-                onChange={(e) => setCity(e.target.value)}
-                value={city}
-                name='city'  
-              />
+            <div className={styles.dropdownBlock}>
+              <p>Выберите навыки, которым хотите научиться</p>
+              <DropdownUI 
+                withFilter={true} 
+                isMultiSelect={true} 
+                value={checkboxes} 
+                placeholder='Выберите'
+              >
+                {({ filter }) => {
+                  const filteredOptions = options.filter((option) =>
+                    option.name.toLowerCase().includes(filter.toLowerCase())
+                  );
+
+                  return <>{renderCheckboxes(filteredOptions)}</>;
+                }}
+                </DropdownUI>
+            </div>
+            <div className={styles.dropdownBlock}>
+              <p>Город</p>
+              <DropdownUI 
+                  withFilter={true} 
+                  isMultiSelect={false} 
+                  value={city}
+                  placeholder='Выберите ваш город'
+                >
+                {() => renderCityOptions()}
+              </DropdownUI>
+            </div>
             <div className={styles.buttons}>
               <ButtonUI 
                 type='button' 
