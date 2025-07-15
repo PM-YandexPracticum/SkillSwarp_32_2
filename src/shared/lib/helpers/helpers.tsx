04b1bd09f6daee5b88415e6234b05abd6b66db7f
RilPax <RilPax@yandex.ru>
2025-07-15 21:06:45 +0500

    lint-fix

--- src/shared/lib/helpers/helpers.tsx
+++ src/shared/lib/helpers/helpers.tsx
@@ -1,6 +1,6 @@
 //файл для хранения вспомогательных функций для сокращения кода
 
-import type { TCard } from "@/shared/global-types";
+import type { TCard } from '@/shared/global-types';
 
 export const formatAge = (age: number) => {
   const lastTwo = age % 100;
@@ -24,22 +24,22 @@ export const formatAge = (age: number) => {
 // сортировка по популярности
 
 export const sortByPopular = (cards: TCard[]) => {
-  return cards.sort((a, b) => b.likes.length - a.likes.length)
-}
+  return cards.sort((a, b) => b.likes.length - a.likes.length);
+};
 
 // сортировка по новизне
 
 export const sortByNewest = (cards: TCard[]) => {
-  return cards.sort((a,b) => b.createdAt - a.createdAt)
-}
+  return cards.sort((a,b) => b.createdAt - a.createdAt);
+};
 
 // сортировка для рекомендаций
 
 export const sorByRecomended = (cards: TCard[]) => {
-  const result = [...cards]
+  const result = [...cards];
   for (let i = result.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
-    ;[result[i], result[j]] = [result[j], result[i]]
+    ;[result[i], result[j]] = [result[j], result[i]];
   }
-  return result
-}
+  return result;
+};
