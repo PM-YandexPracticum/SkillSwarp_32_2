cd22b3da9ed17851f3bc12b06bb0ea582b0c3805
Вера Багрова <verabagrova@Mac-miniMark.local>
2025-07-15 21:03:48 +0300

    correct appheader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import { Register } from '@/pages/register';
 import { Error404 } from '@/pages/404-error';
 import { SkillPage } from '@/pages/skill-page';
 import { Test } from '@/pages/test';
-// import { AppHeaderUI } from '@/shared/ui/app-headerUI/app-header';
 import { Footer } from '@/shared/ui/footer';
 import { AppHeader } from '@/widgets/app-header';
 
@@ -20,15 +19,6 @@ function App() {
   return (
     <>
       <AppHeader />
-      {/* <AppHeaderUI
-        onSkillsClick={() => {}}
-        onToggleTheme={() => {}}
-        onNotificationClick={() => {}}
-        onLikeClick={() => {}}
-        onClearButtonClick={() => {}}
-        user={undefined}
-        // user={{ name: 'Мария', image: 'https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=761&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }}
-      /> */}
       <Routes location={backgroundLocation || location}>
         {/* пока смог выдеить только эти роуты. если найду еще - добавлю */}
         <Route path='/' element={<Main />} />
